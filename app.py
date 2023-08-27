0c98b30c3c81562bb2b61ff16e9b2fa164d0313e
saiganesht15 <saiganesh.t15@gmail.com>
2023-08-27 17:25:47 +0530

    files added - AWS

--- app.py
+++ app.py
@@ -8,17 +8,20 @@ ip_address = socket.gethostbyname(hostname)
 # app = Flask(__name__, template_folder="templates", static_folder="static")
 app = Flask(__name__)
 
-@app.route('/')
-def show_results():
+op = ["India", "Pakistan", "Nepal", "Srilanka", "Afghanistan", "Bangladesh"]
 
+
+@app.route("/")
+def show_results():
     op = ["India", "Pakistan", "Nepal", "Srilanka", "Afghanistan", "Bangladesh"]
+
     def fetch_vote_counts():
         db_params = {
-            'dbname': 'postgres',
-            'user': 'postgres',
-            'password': '3636',
-            'host': 'localhost',
-            'port': '5432'
+            "dbname": "postgres",
+            "user": "postgres",
+            "password": "postgres",
+            "host": "db",
+            "port": "5432",
         }
 
         conn = psycopg2.connect(**db_params)
@@ -29,16 +32,17 @@ def show_results():
         cur.execute(query)
 
         total_votes = 0
-        vote_data = []  # List to store vote option data
-        length_op = len(op)
+        vote_data = []
 
-        for row, country_name in zip(cur, op):
+        for row in cur:
             vote_option, count = row
             total_votes += count
-            vote_data.append({
-                'option': country_name,
-                'count': count,
-            })
+            vote_data.append(
+                {
+                    "option": op[ord(vote_option) - ord('a')],
+                    "count": count,
+                }
+            )
 
         cur.close()
         conn.close()
@@ -50,26 +54,36 @@ def show_results():
 
     total_votes, vote_data = fetch_vote_counts()
 
-# Calculate percentages based on the corrected total_votes value
+    # Calculate percentages based on the corrected total_votes value
     for entry in vote_data:
-        entry['percentage'] = calculate_percentage(entry['count'], total_votes)
+        entry["percentage"] = calculate_percentage(entry["count"], total_votes)
 
-    # Calculate the sum of all vote percentages
-    sum_percentages = sum(entry['percentage'] for entry in vote_data)
+    # Prepare data for rendering
+    prepared_vote_data = []
+    for country_name in op:
+        index = next(
+            (i for i, entry in enumerate(vote_data) if entry["option"] == country_name),
+            None,
+        )
+        count = vote_data[index]["count"] if index is not None else 0
+        prepared_vote_data.append(
+            {
+                "option": country_name,
+                "count": count,
+                "percentage": calculate_percentage(count, total_votes),
+            }
+        )
 
-    # Printing the corrected vote data and sum of percentages
-    print("Vote Results:")
-    for entry in vote_data:
-        print(f"Vote option: {entry['option']}, Count: {entry['count']}, Percentage: {entry['percentage']}%")
+    sum_percentages = sum(entry["percentage"] for entry in prepared_vote_data)
 
-    print("Total Votes:", total_votes)
-    print("Sum of Percentages:", sum_percentages)
+    return render_template(
+        "index.html",
+        vote_data=prepared_vote_data,
+        total_votes=total_votes,
+        sum_percentages=sum_percentages,
+        hostname=hostname,
+    )
 
-    # Now you can access the corrected vote_data list and the sum_percentages value for future use.
 
-    print("HELLOW WORLD")
-
-    return render_template('index.html', vote_data=vote_data, total_votes=total_votes, sum_percentages=sum_percentages, hostname=hostname)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True, threaded=True)
